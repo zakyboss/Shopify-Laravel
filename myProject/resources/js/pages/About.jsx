bf089abb8f9378effc6882bf0ab051551a67d6de
Zakyboss <Boochimo8@gmail.com>
2025-05-20 22:04:31 +0300

    Progress

--- myProject/resources/js/pages/About.jsx
+++ myProject/resources/js/pages/About.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Nav from '../components/ui/Nav'
 
 export default function About() {
   return (
     <div>
+        <Nav/>
         <h1>About Page Zakyboss !</h1>
     </div>
   )
